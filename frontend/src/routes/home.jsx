d70e91670b850649e8e42a1de6845adac1db7074
Yousef <theyousefkhan@gmail.com>
2023-11-04 16:40:14 -0700

    Cleanup

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -5,7 +5,7 @@ export default function home() {
     return (
       <>
         <h1>Welcome To</h1>
-        <h1 class = "Title">Petrpedia!</h1>
+        <h1 class = "title">Petrpedia!</h1>
         <p>Petrpedia is a ultimate hub for Petr fans, with features to request trades, </p>
       </>
     );
