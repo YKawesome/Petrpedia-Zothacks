e292e44c942c2e6356e7844f091fc821aa82266f
Xavier <xachuen@gmail.com>
2023-11-04 16:43:54 -0700

    jeff

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -6,7 +6,14 @@ export default function home() {
       <>
         <h1>Welcome To</h1>
         <h1 class = "Title">Petrpedia!</h1>
-        <p>Petrpedia is a ultimate hub for Petr fans, with features to request trades, </p>
+        <p>Petrpedia is a ultimate hub for Petr fans, with features to request trades, browse Petrs and contribute to the ever-growing online community.</p>
+        <button>Login</button>
+        <button>+ Create Template</button>
+
+        <footer>
+          <h3>Contact</h3>
+      
+        </footer>
       </>
     );
   }
