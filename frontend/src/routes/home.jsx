5eb2ba148e605da2c5b0ff8b584aaac9b2ec44ba
Yousef <theyousefkhan@gmail.com>
2023-11-04 17:43:07 -0700

    Main Page

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -4,16 +4,16 @@ import { Outlet, Link } from "react-router-dom";
 export default function home() {
     return (
       <>
-        <div class="topnav">
+        <header>
           <input type="text" placeholder="Search.."></input>
           <a class="active" href="#home">Home</a>
           <a href="#news">News</a>
           <a href="#contact">Contact</a>
           <a href="#about">About</a>
-        </div>
-        <h1>Welcome To</h1>
-        <h1 class = "title">Petrpedia</h1>
-        <p>Petrpedia is a ultimate hub for Petr fans, with features to request trades, browse Petrs and contribute to the ever-growing community online.</p>
+        </header>
+        <h1 class = "welcome">Welcome To</h1>
+        <h1 class = "title">PetrPedia</h1>
+        <p>PetrPedia is a ultimate hub for Petr fans, with features to request trades, browse Petrs and contribute to the ever-growing community online.</p>
         <button>Login</button>
         <button>+ Create Template</button>
         <br></br>
