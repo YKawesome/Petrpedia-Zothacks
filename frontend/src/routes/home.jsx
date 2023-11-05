29eab565a975db48e24ee29f5543e0090642dc15
Yousef <theyousefkhan@gmail.com>
2023-11-04 17:17:03 -0700

    Main Page CSS

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -5,7 +5,7 @@ export default function home() {
     return (
       <>
         <h1>Welcome To</h1>
-        <h1 class = "Title">Petrpedia!</h1>
+        <h1 class = "title">Petrpedia</h1>
         <p>Petrpedia is a ultimate hub for Petr fans, with features to request trades, browse Petrs and contribute to the ever-growing community online.</p>
         <button>Login</button>
         <button>+ Create Template</button>
