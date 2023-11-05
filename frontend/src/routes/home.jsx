d5e9497e4d8bf9f66f865f719c64555b2b1a2161
Annabel <149740503+wang-annabel@users.noreply.github.com>
2023-11-04 19:24:27 -0700

    editing pages, showing images

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -14,9 +14,10 @@ export default function home() {
           </a>
           
         </header>
+
         <h1 class = "welcome">Welcome To</h1>
         <h1 class = "title">Petrpedia</h1>
-        <p>Petrpedia is a ultimate hub for Petr fans, with features to request trades, browse Petrs and contribute to the ever-growing community online.</p>
+        <p>Petrpedia is an ultimate hub for Petr fans, with features to request trades, browse Petrs and contribute to the ever-growing community online.</p>
         <button class="login">Login</button>
         <a href='/new_template'><button class="createTemplate">+ Create Template</button></a>
         <br></br>
