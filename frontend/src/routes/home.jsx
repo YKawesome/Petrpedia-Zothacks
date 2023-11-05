250615c6c95834e4f0cd4bb3f60d1cd09ac2e9ba
Xavier <xachuen@gmail.com>
2023-11-04 18:06:13 -0700

    yur

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -5,12 +5,12 @@ export default function home() {
     return (
       <>
         <header>
-          <input type="text" placeholder="Search.."></input>
-          <a href="#profile">
-            <img src = "https://static.thenounproject.com/png/2729266-200.png" height = "40"></img>
+          <input class = "headerEle" type="text" placeholder="Search.."></input>
+          <a class = "headerEle" href="#profile">
+            <img class = "circle" src = "https://static.thenounproject.com/png/2729266-200.png" height = "40"></img>
           </a>
-          <a href="#profile">
-            <img src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" height = "40"></img>
+          <a class = "headerEle" href="#profile">
+            <img class = "circle" src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" height = "40"></img>
           </a>
           
         </header>
