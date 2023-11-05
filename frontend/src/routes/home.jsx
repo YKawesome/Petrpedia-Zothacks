9de63097ee4ccb4c7060747e3cdbf5140294e1ca
Xavier <xachuen@gmail.com>
2023-11-04 20:25:37 -0700

    raw

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -5,7 +5,11 @@ export default function home() {
     return (
       <>
         <header>
+          <div class = "hTitle">Petrpedia</div>
           <input class = "headerEle" type="text" placeholder="Search.."></input>
+          <a class = "headerEle" href="#profile">
+            <img class = "circle" src = "https://cdn-icons-png.flaticon.com/512/25/25694.png" height = "40"></img>
+          </a>
           <a class = "headerEle" href="#profile">
             <img class = "circle" src = "https://static.thenounproject.com/png/2729266-200.png" height = "40"></img>
           </a>
@@ -14,16 +18,15 @@ export default function home() {
           </a>
           
         </header>
-        <h1 class = "welcome">Welcome To</h1>
-        <h1 class = "title">Petrpedia</h1>
-        <p>Petrpedia is a ultimate hub for Petr fans, with features to request trades, browse Petrs and contribute to the ever-growing community online.</p>
-        <button class="login">Login</button>
-        <button class="createTemplate">+ Create Template</button>
-        <br></br>
-        <footer>
-          <h3>Contact</h3>
-
-        </footer>
+        <div id = "page">
+          <div id = "content">
+            <h1 class = "welcome">Welcome To</h1>
+            <h1 class = "title">Petrpedia!</h1>
+            <p>Petrpedia is a ultimate hub for Petr fans, with features to request trades, browse Petrs and contribute to the ever-growing community online.</p>
+            <button class="login">Login</button>
+            <button class="createTemplate">+ Create Template</button>
+          </div>
+        </div>
       </>
     );
   }
